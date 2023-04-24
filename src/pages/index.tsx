5fb31e9d6c18094ec2ca14924fe0be0fd842cc15
Kyle Stubblefield <iamtuxmeister@gmail.com>
2023-04-23 18:32:24 -0700

    logging and router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
+
+  console.log(data);
   return (
     <>
       <Head>
@@ -18,7 +19,8 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
       <div>
-      {!user.isSignedIn && <SignInButton />}{!!user.isSignedIn && <SignOutButton />}
+      {!user.isSignedIn && <SignInButton />}
+      {!!user.isSignedIn && <SignOutButton />}
       </div>
 
       </main>
